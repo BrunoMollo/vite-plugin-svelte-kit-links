9b2f02776d82b0d7aefaa998bd5ed45cfbbd4d2f
BrunoMollo <dia.mollo.bruno@gmail.com>
2024-03-16 17:13:53 -0300

    change folder

--- src/index.ts
+++ src/index.ts
@@ -35,7 +35,7 @@ export function svelteKitLinks() {
       server.watcher.on("change", async () => {
         const routes = await printFilesRecursively("src/routes");
         fs.writeFile(
-          "./node_modules/svelte-links/index.d.ts",
+          "./node_modules/vite-plugin-svelte-kit-links/index.ts",
           "export type SvelteKitLink = " +
             routes.map((x) => `\`${x}\``).join("|")
         );
